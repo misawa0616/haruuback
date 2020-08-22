ed8f3ffbcccbde48404a24d8231c244598bf66c3
misawa takuma <takumajane1@outlook.jp>
2020-08-22 20:42:14 +0900

    hoge

--- api/views.py
+++ api/views.py
@@ -60,7 +60,7 @@ class ChangeEmailAPIView(APIView):
         serializer.save()
         send_mail(
             'mail address confirm',
-            'http://127.0.0.1:8000/api/v1/'
+            'http://favoritetag.xyz//api/v1/'
             'confirm_change_email/{}'.format(serializer.data.get('token')),
             'takumajane1@outlook.jp',
             [serializer.data.get('after_change_email')],
